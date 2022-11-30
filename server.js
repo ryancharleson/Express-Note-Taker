1db535ef456af5a76a3cbed937736fb26acce663
hrsprs <jamescharleson@yahoo.com>
2022-11-30 12:26:13 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+
+
+const path = require('path');
+const api = require('./routes');
+
+
+// Requireing express for our routes
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
